38f29a5bdc1feeb77f60bc303ebc275d89fd9d7a
robinscholz <dev@studioscholz.info>
2023-01-02 14:54:44 +0100

    chore: initial setup and cleanup

---
+++ packages/teaser/src/settings.ts
@@ -0,0 +1,38 @@
+import {
+  DropdownSize,
+  IconEnum,
+  defineSettings,
+} from '@frontify/guideline-blocks-settings'
+
+export const settings = defineSettings({
+  main: [
+    {
+      id: 'main-dropdown',
+      type: 'dropdown',
+      defaultValue: 'content_block',
+      size: DropdownSize.Large,
+      disabled: true,
+      choices: [
+        {
+          value: 'content_block',
+          icon: IconEnum.BuildingBlock,
+          label: 'Content Block',
+        },
+      ],
+    },
+  ],
+  style: [
+    {
+      id: 'color',
+      label: 'Text Color',
+      type: 'colorInput',
+      defaultValue: {
+        red: 113,
+        green: 89,
+        blue: 215,
+        alpha: 1,
+        name: 'Frontify Violet',
+      },
+    },
+  ],
+})
