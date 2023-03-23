2e4b11ea032eb99289dc19390fc5c950fc914481
David Kaipf <d.kaipf@kms-team.com>
2023-03-23 13:53:03 +0100

    fix: text-colors

--- packages/teaser/src/components/views/LiveView.tsx
+++ packages/teaser/src/components/views/LiveView.tsx
@@ -16,37 +16,45 @@ export const LiveView: FC<BlockProps> = ({ appBridge }) => {
   const containerClassesArray = ['tw-flex tw-flex-col tw-gap-8']
   const headerClassesArray = ['tw-flex tw-flex-col tw-gap-6']
   const teaserClassesArray = ['tw-grid tw-grid-cols-2']
+  const copyClassesArray = ['tw-max-w-full tw-whitespace-pre-line']
 
   let teaser = ''
   let container = ''
   let header = ''
+  let copyStyling = ''
 
   if (backgroundGlobal) {
     if (background === TeaserBackground.Light) {
       container = 'tw-bg-zeiss-gray-2 tw-text-zeiss-gray-19'
       teaser = 'tw-border-t tw-border-white tw-bg-white tw-gap-px'
+      copyStyling = 'tw-text-zeiss-gray-19'
     } else if (background === TeaserBackground.Dark) {
       container = 'tw-bg-zeiss-gray-21 tw-text-zeiss-gray-4'
       teaser =
         'tw-border-t tw-border-zeiss-gray-19 tw-bg-zeiss-gray-19 tw-gap-px'
+      copyStyling = 'tw-text-zeiss-gray-7'
     } else if (background === TeaserBackground.Azure) {
-      container = 'tw-bg-zeiss-azure-90 tw-text-zeiss-gray-4'
+      container = 'tw-bg-zeiss-azure-90 tw-text-white'
       teaser = 'tw-border-t tw-border-white tw-bg-white tw-gap-px'
+      copyStyling = 'tw-text-white'
     }
 
     header = 'tw-pt-3 tw-px-4'
   } else {
     teaser = 'tw-gap-2'
     container = 'tw-text-zeiss-gray-19'
+    copyStyling = 'tw-text-zeiss-gray-16'
   }
 
   containerClassesArray.push(container)
   teaserClassesArray.push(teaser)
   headerClassesArray.push(header)
+  copyClassesArray.push(copyStyling)
 
   const containerClasses = containerClassesArray.join(' ')
   const headerClasses = headerClassesArray.join(' ')
   const teaserClasses = teaserClassesArray.join(' ')
+  const copyClasses = copyClassesArray.join(' ')
 
   const parsedCopy = parseHtml(
     serialize(
@@ -61,11 +69,7 @@ export const LiveView: FC<BlockProps> = ({ appBridge }) => {
       <div className={containerClasses}>
         <div className={headerClasses}>
           <div className="tw-type-headline">{title}</div>
-          {copy && (
-            <div className="tw-max-w-full tw-whitespace-pre-line">
-              {parsedCopy}
-            </div>
-          )}
+          {copy && <div className={copyClasses}>{parsedCopy}</div>}
         </div>
         <div className={teaserClasses}>
           {items?.map((item: Item) => {
